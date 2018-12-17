4cddad0f6e5ace33b76eab777d0943d3609ef9e6
github username <github email address>
2018-12-17 15:54:08 -0500

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,42 +1,48 @@
 require 'pry'
 
 class Artist
-  attr_accessor :name
-  attr_reader :songs
 
-  @@artists = []
+    include Paramable
+    extend Findable
+    extend Memorable::ClassMethods
+    include Memorable::InstanceMethods
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+    attr_accessor :name
+    attr_reader :songs
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+    @@artists = []
 
-  def self.all
-    @@artists
-  end
+    def initialize
+      super
+      @songs = []
+    end
 
-  def self.reset_all
-    self.all.clear
-  end
+    # def self.find_by_name(name)
+    #     @@artists.detect{|a| a.name == name}
+    # end
 
-  def self.count
-    self.all.count
-  end
+    def self.all
+        @@artists
+    end
 
-  def add_song(song)
-    @songs << song
-    song.artist = self
-  end
+    # def self.reset_all
+    #   self.all.clear
+    # end
+    #
+    # def self.count
+    #   self.all.count
+    # end
 
-  def add_songs(songs)
-    songs.each { |song| add_song(song) }
-  end
+    def add_song(song)
+        @songs << song
+        song.artist = self
+    end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+    def add_songs(songs)
+        songs.each { |song| add_song(song) }
+    end
+
+    # def to_param
+    #     name.downcase.gsub(' ', '-')
+    # end
 end
